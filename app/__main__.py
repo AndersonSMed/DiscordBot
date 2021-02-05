b183a025897c0f875cf200c1587200016d0a1498
Anderson Medeiros <anderson.santos.medeiros@hotmail.com>
2021-02-04 22:04:39 -0300

    Changed environment name

--- app/__main__.py
+++ app/__main__.py
@@ -5,12 +5,12 @@ import discord
 from exceptions import NoEnvironmentVariableError
 
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-if not DISCORD_TOKEN:
+if not DISCORD_BOT_TOKEN:
     raise NoEnvironmentVariableError(
-        "Please, set your DISCORD_TOKEN environment variable"
+        "Please, set your DISCORD_BOT_TOKEN environment variable"
     )
     exit()
 
-client = discord.Client(DISCORD_TOKEN)
+client = discord.Client(DISCORD_BOT_TOKEN)
